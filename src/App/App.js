f3c688b5ea93fc2d5353da77ca38f5d81ff1f407
haki_ainz <seva.pantiukhin@yandex.ru>
2023-10-18 20:13:58 +0400

    Variations on Event Handlers

--- src/App/App.js
+++ src/App/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import AnimalShow from '../components/AnimalShow/AnimalShow';
 
 const App = () => {
-  const handleClick = () => {
-    console.log('button was clicked');
-  };
+  // const handleClick = () => {
+  //   console.log('button was clicked');
+  // };
 
   return (
     <div>
-      <button onClick={handleClick}>Add Animal</button>
+      <button onClick={() => console.log('Button was clicked!')}>
+        Add Animal
+      </button>
     </div>
   );
 };
